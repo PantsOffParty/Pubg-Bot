6ca9ffa27ae2961cabb0077dd437a502500af905
Case <case.webb@gmail.com>
2018-11-03 03:52:35 -0500

    Add all coordinates win map

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 import javax.imageio.ImageIO;
 import javax.security.auth.login.LoginException;
@@ -10,12 +12,14 @@ import java.util.*;
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
+import java.util.List;
 
 
 public class Main extends ListenerAdapter {
 
     private final String tempDir = System.getProperty("java.io.tmpdir"); //Stores output images
     private Point currentCoordinates = new Point(); //Stores Current Point for win recording
+    private char currentMap = ' ';
     private final Random rand = new Random(); //Random generator for coordinate generation
 
     //Stuff for Strategy generation. pulled out so it doesn't rerun every time a message is received
@@ -45,14 +49,15 @@ public class Main extends ListenerAdapter {
 
     private final int STRATNUM = strat.length;
     private static Map<String, String> helpMap; //Map to store command list
-    static
-    {
+
+    static {
         helpMap = new HashMap<>();
         helpMap.put("!ping", "Check if the bot is online.");
         helpMap.put("!win", "Save winning map position to the server.");
         helpMap.put("!strategy", "Be given a random strategy for how to play out the next round.");
         helpMap.put("!drop (e,m,s) OR !", "Be given a random position to drop in the next round.");
         helpMap.put("!help", "View all possible bot commands.");
+        helpMap.put("!allwin (e,m,s)", "Display a map of all starting coordinates that resulted in a win for a given map.");
     }
 
     //Logs bot into discord
@@ -74,7 +79,7 @@ public class Main extends ListenerAdapter {
 
     //Bot responses to messages
     @Override
-    public void onMessageReceived(MessageReceivedEvent event){
+    public void onMessageReceived(MessageReceivedEvent event) {
 
         //Outputs message author and content to terminal
         System.out.println("We received a message from " +
@@ -97,7 +102,7 @@ public class Main extends ListenerAdapter {
 
         //Stores winning map image AND winning coordinates in file
         if (messageText.equals("!win")) {
-            exportWinningDropImage();
+            exportWinningDropCoordinates();
             event.getChannel().sendMessage("Winning coordinates have been saved!").queue();
         }
 
@@ -112,21 +117,26 @@ public class Main extends ListenerAdapter {
         if (messageText.contains("!drop") || messageText.equals("!")) {
             String cmdSplit[] = messageText.split(" ", 2);
             BufferedImage img;
-            if (cmdSplit.length != 2) {
+            if (cmdSplit.length != 2 || messageText.equals("!")) {
                 img = getImageFromResource("PUBGMAP1.jpg");
+                currentMap = 's';
             } else {
                 switch (cmdSplit[1]) {
                     case "s":
                         img = getImageFromResource("PUBGMAP1.jpg");
+                        currentMap = 's';
                         break;
                     case "m":
                         img = getImageFromResource("PUBGMAP2.jpg");
+                        currentMap = 'm';
                         break;
                     case "e":
                         img = getImageFromResource("PUBGMAP3.jpg");
+                        currentMap = 'e';
                         break;
                     default:
                         img = getImageFromResource("PUBGMAP1.jpg");
+                        currentMap = 's';
                         break;
                 }
             }
@@ -135,11 +145,25 @@ public class Main extends ListenerAdapter {
             event.getChannel().sendFile(writeOutputFile(img)).queue();
         }
 
-        //Outputs command list to discord
-        if (messageText.equals("!help")){
-            StringBuilder messageToSend = new StringBuilder();
-            for(Map.Entry<String, String> command : helpMap.entrySet())
+        //display all winning coordinates for a given map
+        if (messageText.contains("!allwin"))
+        {
+            String mapKey = "s";
+            if (messageText.split(" ").length != 1)
             {
+                mapKey = messageText.split(" ")[1];
+            }
+            try {
+                event.getChannel().sendFile(writeOutputFile(getAllWinCoordinatesImage(mapKey))).queue();
+            } catch (IOException e) {
+                event.getChannel().sendMessage("You have not captured a win for this map because you suck.").queue();
+            }
+        }
+
+        //Outputs command list to discord
+        if (messageText.equals("!help")) {
+            StringBuilder messageToSend = new StringBuilder();
+            for (Map.Entry<String, String> command : helpMap.entrySet()) {
                 messageToSend.append(command.getKey())
                         .append(" : ")
                         .append(command.getValue())
@@ -147,9 +171,9 @@ public class Main extends ListenerAdapter {
             }
             event.getChannel().sendMessage(messageToSend.toString()).queue();
         }
-
     }
 
+    //
     private BufferedImage getImageFromResource(String image) {
         try {
             return ImageIO.read(this.getClass().getResourceAsStream(image));
@@ -189,12 +213,67 @@ public class Main extends ListenerAdapter {
         return outputFile;
     }
 
-    private void exportWinningDropImage() {
-        File winningStart = new File(tempDir + "PUBGMAPEDIT.jpg");
-        File export = new File(System.getenv("USERPROFILE") +
-                "\\desktop\\PUBGWin-X" + currentCoordinates.getX() + "-Y" + currentCoordinates.getY() + ".jpg");
+    private void exportWinningDropCoordinates()
+    {
+        String mapFileName = "";
+        switch (currentMap) {
+            case 's':
+                mapFileName = "WinCoordinatesSanhok";
+                break;
+            case 'e':
+                mapFileName = "WinCoordinatesErangel";
+                break;
+            case 'm':
+                mapFileName = "WinCoordinatesMiramar";
+                break;
+        }
+        try (BufferedWriter output = new BufferedWriter(new FileWriter(System.getenv("USERPROFILE") +
+                "\\desktop\\" + mapFileName + ".txt", true))) {
+            output.write(currentCoordinates.getX() + "," + currentCoordinates.getY());
+            output.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        //noinspection ResultOfMethodCallIgnored
-        winningStart.renameTo(export);
+    private BufferedImage getAllWinCoordinatesImage(String mapKey) throws IOException {
+        String mapFileName;
+        String mapImageName;
+        BufferedImage image;
+        switch (mapKey) {
+            case "e":
+                mapFileName = "WinCoordinatesErangel";
+                mapImageName = "PUBGMAP3.jpg";
+                break;
+            case "m":
+                mapFileName = "WinCoordinatesMiramar";
+                mapImageName = "PUBGMAP2.jpg";
+                break;
+            default:
+                mapFileName = "WinCoordinatesSanhok";
+                mapImageName = "PUBGMAP1.jpg";
+                break;
+        }
+        image = getImageFromResource(mapImageName);
+        try (BufferedReader br = new BufferedReader(new FileReader(System.getenv("USERPROFILE") +
+                "\\desktop\\" + mapFileName + ".txt")))
+        {
+            for (String line; (line = br.readLine()) != null; )
+            {
+                List<String> x_y_coords = Arrays.asList(line.split(","));
+                int x = (int) Double.parseDouble(x_y_coords.get(0));
+                int y = (int) Double.parseDouble(x_y_coords.get(1));
+
+                assert image != null;
+                Graphics2D graphics2D = image.createGraphics();
+                graphics2D.setFont(new Font("Ariel", Font.PLAIN, 50));
+                graphics2D.setColor(Color.RED);
+                graphics2D.drawString("x", x, y);
+            }
+        } catch (IOException e)
+        {
+            throw new IOException();
+        }
+        return image;
     }
 }
