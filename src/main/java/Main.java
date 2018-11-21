4c19145550d9607e9da8a0e2dd7818a3bb9df158
Case <case.webb@gmail.com>
2018-11-20 18:19:36 -0600

    Fix main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,7 @@
-import Pubg.Api.Client.PubgApiClient;
-
 public class Main {
 
     //Runs discord bot object
     public static void main(String[] args) throws Exception {
-        //new DiscordBotMessageHandler();
-
-        PubgApiClient client = new PubgApiClient();
-        //client.getPlayerIdFromName("CuteBabyVagina");
-
-        System.out.println(client.getLatestSeasonId());
-
+        new DiscordBotMessageHandler();
     }
 }
