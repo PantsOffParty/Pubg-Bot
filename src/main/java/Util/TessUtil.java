6df51facf9bde1b6fe952fa1591caac46240ed8d
MyMan <ryanhabben1@gmail.com>
2020-01-13 21:24:40 -0500

    Revert "Merge remote-tracking branch 'origin/OcrAttempt' into OcrAttempt"
    This reverts commit c9da5c6c2641330b98d0e129e6a64cde1fb22b3d.

--- src/main/java/Util/TessUtil.java
+++ src/main/java/Util/TessUtil.java
@@ -15,10 +15,11 @@ public class TessUtil {
     public static Point getDropPositionCoords(BufferedImage mapImg, String location) {
         Tesseract tesseract = new Tesseract();
         tesseract.setDatapath(Paths.get("src", "main", "resources", "trainedData").toAbsolutePath().toString());
-        tesseract.setTessVariable("user_defined_dpi", "300");
+        tesseract.setTessVariable("user_defined_dpi", "500");
+        tesseract.setTessVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         Rectangle rect = new Rectangle();
 
-        mapImg = ImageHelper.getScaledInstance(mapImg, 1000, 1000);
+   //     mapImg = ImageHelper.getScaledInstance(mapImg, 2000, 2000);
         try {
             System.out.println(tesseract.doOCR(mapImg));
         } catch (TesseractException e) {
